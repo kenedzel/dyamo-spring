89e57db0be59612a98a36ca38157312e6c9a574e
kenneth <kenneth.azarcon@novare.com.hk>
2017-12-22 14:01:15 +0800

    Added CRUD functionality with Paging and Sorting Repository.

--- src/main/java/com/dynamodb/demo/dynamodbdemo/controller/ProductInfoController.java
+++ src/main/java/com/dynamodb/demo/dynamodbdemo/controller/ProductInfoController.java
@@ -5,10 +5,10 @@ import com.dynamodb.demo.dynamodbdemo.repository.ProductInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ProductInfoController {
@@ -24,6 +24,21 @@ public class ProductInfoController {
     @RequestMapping(value = "/product", method = RequestMethod.GET)
     public ResponseEntity<Iterable<ProductInfo>> getProducts() {
         return new ResponseEntity<>(productInfoRepository.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/product/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Map<String,String>> deleteProduct(@PathVariable("id") String id) {
+        Map<String, String> map = new HashMap<>();
+        productInfoRepository.delete(id);
+        map.put("status", "Deleted data");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/product/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<ProductInfo> updateProduct(@PathVariable("id") String id, @RequestBody ProductInfo productInfo) {
+        ProductInfo productInfoUpdated = productInfoRepository.findOne(id);
+        productInfoUpdated.setCost(productInfo.getCost());
+        return new ResponseEntity<>(productInfoRepository.save(productInfoUpdated), HttpStatus.OK);
     }
 
 }
